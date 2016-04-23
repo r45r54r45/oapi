6ef39cef8c29bdf8a6e15a580c04fbfe379bb71f
Woohyun Kim <r45r54r45@naver.com>
2016-04-24 01:22:54 +0900

    kk

--- github.php
+++ github.php
@@ -1,3 +1,4 @@
 <?php `git pull origin master`;?>
-<?php `find -type d -exec chmod 755 {} \;`; ?>
-<?php `find -type f -exec chmod 644 {} \;`; ?>
+
+<!-- find -type d -exec chmod 755 {} \; -->
+<!-- find -type f -exec chmod 644 {} \; -->
